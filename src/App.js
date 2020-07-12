d292a75d24e8a74e41eff1472a92104f1cf696f0
Jonathan Zach <jonatanz@gmail.com>
2020-07-12 15:32:16 +0300

    added firebase utils and included ability to store authenticated users into my firestore db

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import "./App.css";
 
 class App extends React.Component {
@@ -18,8 +18,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null; // close subscription to avoid memory leaks when unmount
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user }); // return value is firebase.Unsubscribe
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        // if the user signs in
+        const userRef = await createUserProfileDocument(userAuth); // if there's document i'll get it, else the function will create it and i'll get it
+
+        // listen to changes in the data, and anyways I get the first state if that data (snapShot)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth }); // if the user signs out
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,10 +15,37 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; // if ther user is null (doesn't exist)
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    // create the data
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" }); // triggers the Google pop-up whenever someone uses the Google auth provider for authentication/sign in
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
 export default firebase;
